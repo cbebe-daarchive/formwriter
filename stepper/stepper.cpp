ac6682cc0063a86e20b19caf26376b8ebeb1b045
charlesancheta <cancheta@ualberta.ca>
2020-01-18 16:52:10 -0700

    stepper motor attempt failed

---
+++ stepper/stepper.cpp
@@ -0,0 +1,48 @@
+#include <Arduino.h>
+
+void setup() {  
+  init();
+  for (int i = 4; i < 8; ++i) {
+    pinMode(i, INPUT);
+    digitalWrite(i, LOW);
+  }
+  Serial.begin(9600);
+}
+
+int8_t getHexValue(char digit) {
+	// converting char to their ASCII value without using int() function
+	uint8_t hexNum = digit - '\0';
+	// 0 to 9
+	if (48 <= hexNum && hexNum <= 57) {
+    	return hexNum - 48;
+	// A to F
+	} else if (65 <= hexNum && hexNum <= 70) {
+    	return hexNum - 55;
+	// a to F
+	} else if (97 <= hexNum && hexNum <= 102) {
+    	return hexNum - 87;
+	} else {
+    	return -1;
+	}
+    
+}
+
+int main() {
+  setup();
+  while (true) {
+    if (Serial.available() > 0) {
+      Serial.print("Read: ");
+      char digit = Serial.read();
+      int8_t hexNum = getHexValue(digit);
+      Serial.println(hexNum);
+      if (hexNum > 0) {
+        for (int j = 0; j < 4; j++) {
+          digitalWrite(j + 4, hexNum & (1 << j));
+        }  
+      }
+      
+    }
+  } 
+  Serial.flush();
+  return 0;
+}
