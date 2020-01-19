364189e538db2ba73d724615038037966b475a51
charlesancheta <cancheta@ualberta.ca>
2020-01-19 04:13:17 -0700

    uploaded working driver

---
+++ uln2003/uln2003.ino
@@ -0,0 +1,36 @@
+#include <AccelStepper.h>
+#define HALFSTEP 8
+
+#define STEPPER_MAX_SPEED 3000
+#define STEPPER_FAST STEPPER_MAX_SPEED
+#define STEPPER_SLOW (STEPPER_MAX_SPEED/2)
+
+// Motor pin definitions
+#define PIN_MOTOR1_IN1  8     // IN1 on the ULN2003 driver 1
+#define PIN_MOTOR1_IN2  9     // IN2 on the ULN2003 driver 1
+#define PIN_MOTOR1_IN3  10    // IN3 on the ULN2003 driver 1
+#define PIN_MOTOR1_IN4  11    // IN4 on the ULN2003 driver 1
+
+AccelStepper stepperX(HALFSTEP, PIN_MOTOR1_IN1, PIN_MOTOR1_IN3, PIN_MOTOR1_IN2, PIN_MOTOR1_IN4);
+
+void setup() {
+
+  stepperX.setMaxSpeed(STEPPER_MAX_SPEED);
+  stepperX.setAcceleration(300.0);
+  stepperX.setSpeed(STEPPER_FAST);
+      stepperX.enableOutputs();
+//  stepperX.disableOutputs();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  stepperX.moveTo(5000);
+      if (abs(stepperX.distanceToGo()) > 1000)
+        while (abs(stepperX.distanceToGo()) > 1000) stepperX.run();
+      else if (abs(stepperX.distanceToGo()) > 100)
+        while (abs(stepperX.distanceToGo()) > 100) stepperX.run();
+      else if (abs(stepperX.distanceToGo()) > 50)
+        while (abs(stepperX.distanceToGo()) > 50) stepperX.run();
+      else
+        stepperX.run();
+}
