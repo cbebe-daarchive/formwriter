dca8b14c199c4ade372ae88d5b8aec1618fbaf75
charlesancheta <cancheta@ualberta.ca>
2020-01-19 10:47:26 -0700

    update btcont

--- btncont/btncont.ino
+++ btncont/btncont.ino
@@ -6,25 +6,25 @@
 #define STEPPER_SLOW (STEPPER_MAX_SPEED/2)
 
 // Motor pin definitions
-#define PIN_MOTOR1_IN1  8     // IN1 on the ULN2003 driver 1
-#define PIN_MOTOR1_IN2  9     // IN2 on the ULN2003 driver 1
-#define PIN_MOTOR1_IN3  10    // IN3 on the ULN2003 driver 1
-#define PIN_MOTOR1_IN4  11    // IN4 on the ULN2003 driver 1
+#define PIN_MOTOR1_IN1  0     // IN1 on the ULN2003 driver 1
+#define PIN_MOTOR1_IN2  1     // IN2 on the ULN2003 driver 1
+#define PIN_MOTOR1_IN3  2    // IN3 on the ULN2003 driver 1
+#define PIN_MOTOR1_IN4  3    // IN4 on the ULN2003 driver 1
 
 #define PIN_MOTOR2_IN1  4     // IN1 on the ULN2003 driver 2
 #define PIN_MOTOR2_IN2  5     // IN2 on the ULN2003 driver 2
 #define PIN_MOTOR2_IN3  6    // IN3 on the ULN2003 driver 2
 #define PIN_MOTOR2_IN4  7    // IN4 on the ULN2003 driver 2
 
-AccelStepper stepperX(HALFSTEP, PIN_MOTOR1_IN1, PIN_MOTOR1_IN3, PIN_MOTOR1_IN2, PIN_MOTOR1_IN4);
-AccelStepper stepperY(HALFSTEP, PIN_MOTOR2_IN1, PIN_MOTOR2_IN3, PIN_MOTOR2_IN2, PIN_MOTOR2_IN4);
+AccelStepper stepperX(HALFSTEP, PIN_MOTOR1_IN1, PIN_MOTOR1_IN2, PIN_MOTOR1_IN3, PIN_MOTOR1_IN4);
+AccelStepper stepperY(HALFSTEP, PIN_MOTOR2_IN1, PIN_MOTOR2_IN2, PIN_MOTOR2_IN3, PIN_MOTOR2_IN4);
 
 
 void setup() {
-  for (int i = 22; i < 26; i++)
-  {
-    digitalWrite(i, HIGH);
-  }
+//  for (int i = 22; i < 26; i++)
+//  {
+//    digitalWrite(i, HIGH);
+//  }
   
   
   stepperX.setMaxSpeed(STEPPER_MAX_SPEED);
@@ -40,21 +40,21 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (digitalRead(22) == LOW) {
+  //   put your main code here, to run repeatedly:
+  if (digitalRead(3) == LOW) {
     stepperX.move(10);
     stepperX.run();
   } 
-  if (digitalRead(23) == LOW) {
-    stepperX.move(10);
+  if (digitalRead(2) == LOW) {
+    stepperX.move(-10);
     stepperX.run();
   }
 
-  if (digitalRead(24) == LOW) {
+  if (digitalRead(1) == LOW) {
     stepperY.move(10);
     stepperY.run();
   } 
-  if (digitalRead(25) == LOW) {
+  if (digitalRead(0) == LOW) {
     stepperX.move(-10);
     stepperY.run();
   }
